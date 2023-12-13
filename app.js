5e8093aebd7f071847e27b0ea921a475f7726ecd
shetalw <sdwaters8@gmail.com>
2023-12-13 17:06:38 -0500

    A6 Completed with sussessful working code

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import session from "express-session";
 import "dotenv/config";
 import Hello from "./hello.js"
 import Lab5 from "./Lab5.js";
@@ -9,20 +10,52 @@ import AssignmentRoutes from './assignments/routes.js';
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 
-mongoose.connect(process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas");
 
-const app = express();
-app.use(cors());
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+
+mongoose.connect(CONNECTION_STRING);
+
+const app = express()
+
+app.use(
+    cors({
+        credentials: true, // support cookies
+        origin: function (origin, callback) {
+        
+            if (!origin) return callback(null, true);
+
+            if (process.env.FRONTEND_URL.split(" ").indexOf(origin) === -1) {
+                var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        }
+    })
+);
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+
+app.use(
+    session(sessionOptions)
+);
+
 app.use(express.json());
+
 UserRoutes(app);
 
-
-app.use(express.json());
-UserRoutes(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app)
 Hello(app)
-
 app.listen(process.env.PORT || 4000);
--- users/routes.js
+++ users/routes.js
@@ -1,40 +1,29 @@
 import * as dao from "./dao.js";
 
 
-let currentUser = null;
-
 
 function UserRoutes(app) {
-  const createUser = async (req, res) => { };
-
-  const deleteUser = async (req, res) => { };
-
-
-  const findUserById = async (req, res) => { };
-
-  const signup = async (req, res) => { };
-
-
-  const signout = (req, res) => { };
-
-
 
   const signin = async (req, res) => {
     const { username, password } = req.body;
-    currentUser = await dao.findUserByCredentials(username, password);
+    const currentUser = await dao.findUserByCredentials(username, password);
+    req.session['currentUser'] = currentUser;
     res.json(currentUser);
   };
 
+
   const account = async (req, res) => {
-    res.json(currentUser);
+    res.json(req.session['currentUser']);
   };
 
   const updateUser = async (req, res) => {
     const { userId } = req.params;
     const status = await dao.updateUser(userId, req.body);
-    currentUser = await dao.findUserById(userId);
+    const currentUser = await dao.findUserById(userId);
+    req.session['currentUser'] = currentUser;
     res.json(status);
   };
+
 
   const findAllUsers = async (req, res) => {
     const users = await dao.findAllUsers();
@@ -42,9 +31,38 @@ function UserRoutes(app) {
   };
 
 
+  const createUser = async (req, res) => {
+    const user = await dao.createUser(req.body);
+    res.json(user);
+  };
 
+  const findUserById = async (req, res) => {
+    const user = await dao.findUserById(req.params.userId);
+    res.json(user);
+  };
 
+  const deleteUser = async (req, res) => {
+    const status = await dao.deleteUser(req.params.userId);
+    res.json(status);
+};
 
+const signup = async (req, res) => {
+  const user = await dao.findUserByUsername(
+    req.body.username);
+  if (user) {
+    res.status(400).json(
+      { message: "Username already taken" });
+    return;
+  }
+  const currentUser = await dao.createUser(req.body);
+  req.session['currentUser'] = currentUser;
+  res.json(currentUser);
+};
+
+const signout = (req, res) => {
+  req.session.destroy();
+  res.json(200);
+};
 
 
   app.post("/api/users", createUser);
